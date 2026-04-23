8c557f8272254bf3d36d40b8bc2d9c320223267e
atrocria <normmallys@gmail.com>
2026-04-23 23:29:37 +0800

    now

---
+++ src/main/java/ui/Launcher.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // This tricks the JVM into loading JavaFX properly
+        App.main(args);
+    }
+}
--- src/main/java/ui/Main.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public class Main {
-    public static void main(String[] args) {
-        App.main(args); // Calls the main in your App class
-    }
-}
